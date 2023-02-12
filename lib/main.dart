93c134831a58ab7d4ed01e91786a0a753c72679c
ibhavikmakwana <bhavik@multipl.xyz>
2023-02-12 12:41:47 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: HomePage(),
+    );
   }
 }
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Disney Characters'),
+      ),
+    );
+  }
+}
+
